e6d2effadc05d2bd10d4b8a6bfbf7baa4f265933
The Processor <96575921+elrosyadi296@users.noreply.github.com>
2026-07-10 07:36:12 +0700

    Add files via upload

---
+++ peserta/database.js
@@ -0,0 +1,14 @@
+const database = [
+{nama:"AAQILAH AL SAHWA", asal:"MIS AL YUSUFIYAH BULUSPESANTREN", username:"260216", password:"260216", kelas:"KELAS REGULER"},
+{nama:"AHMAD MAULANA PUTRA ", asal:"SD IT HASANAH FATIMIYAH ", username:"260003", password:"260003", kelas:"KELAS REGULER"},
+{nama:"AMRI FAIZAL MUTAMIM", asal:"MIS SULTAN AGUNG 1 KALIPOH", username:"260223", password:"260223", kelas:"KELAS REGULER"},
+{nama:"ARSYAH SATRIAYADI PUTRA", asal:"MI MA'ARIF JOJOGAN", username:"260215", password:"260215", kelas:"KELAS REGULER"},
+{nama:"AURELIA NAURA ZAFARANI", asal:"SDIT VIP AL-HUDA", username:"260048", password:"260048", kelas:"KELAS REGULER"},
+{nama:"ELVA DINDA APRELIA", asal:"MTS N 2 KULON PROGO", username:"260224", password:"260224", kelas:"KELAS REGULER"},
+{nama:"FAHRI AKBAR", asal:"SDN 1 JOGOSIMO", username:"260226", password:"260226", kelas:"KELAS REGULER"},
+{nama:"MIFTAH SUNANDAR AL ANSORI", asal:"SDN 1 DOROWATI", username:"260225", password:"260225", kelas:"KELAS REGULER"},
+{nama:"MUHAMMAD HABIB AL REVANNO", asal:"UPT SD NEGERI SIMPANG SENDER SELATAN ", username:"260191", password:"260191", kelas:"KELAS REGULER"},
+{nama:"MUTIARA UZMA AL FIRDAUS", asal:"SDN BINTARO 14 PAGI", username:"260217", password:"260217", kelas:"KELAS REGULER"},
+{nama:"RIZQI GUSTI PUTRA", asal:"MIS SULTAN AGUNG 1 KALIPOH", username:"260222", password:"260222", kelas:"KELAS REGULER"},
+{nama:"WASISTO JATI SADONO", asal:"MI NU YAPIKA", username:"260211", password:"260211", kelas:"KELAS REGULER"}
+];
